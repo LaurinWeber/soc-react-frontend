b138e081eb1593053e01cc686d568912ad1afc3b
laurin <laurin.weber@students.hevs.ch>
2021-03-14 20:07:27 +0100

    updated welcome page

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           The webiste is being updated please come back soon.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
       </header>
     </div>
   );
